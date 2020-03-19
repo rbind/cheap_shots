ade4c7ba7be326d5674f27f986e2310660ed36bc
Drew Tyre <atyre2@unl.edu>
2020-03-19 11:01:35 -0500

    first drat post vii

---
+++ content/post/R/jhu_helpers.R
@@ -0,0 +1,50 @@
+require("dplyr")
+require("tidyr")
+require("stringr")
+require("lubridate")
+us_confirmed_long <- function(all_wide){
+  # returns long dataframe with state level data for USA
+  abbrv <- readr::read_csv(here::here("content/post/data/USA_state_abb.csv"))
+  t1 <- all_wide %>% 
+    rename(province = "Province/State", 
+           country_region = "Country/Region") %>% 
+    pivot_longer(-c(province, country_region, Lat, Long), 
+                 names_to = "Date", values_to = "cumulative_cases") %>% 
+    filter(country_region == "US") %>% 
+    mutate(Date = mdy(Date)) %>% 
+    #  mutate(Date = lubridate::mdy(Date) - lubridate::days(1)) %>% 
+    arrange(province, Date)  %>% 
+    # filter out state rows prior to march 8, and county rows after that. 
+    # this is dropping virgin islands ... 
+    filter(str_detect(province, ", ") & Date <= "2020-03-9" |
+             str_detect(province, ", ", negate = TRUE) & Date > "2020-03-9",
+           str_detect(province, "Princess", negate = TRUE)) 
+  # split off the county rows, match with state names and sum
+  sum_counties <- t1 %>% 
+    filter(str_detect(province, ", "),) %>% 
+    separate(province, into = c("county", "ANSI_letter"), ", ") %>% 
+    left_join(y = select(abbrv, ANSI_letter, name)) %>% 
+    rename(province = name) %>% 
+    group_by(province, Date) %>% 
+    summarize(cumulative_cases = sum(cumulative_cases)) %>% 
+    ungroup()
+  states <- t1 %>% 
+    filter(str_detect(province, ", ", negate = TRUE)) %>% 
+    select(province, Date, cumulative_cases)
+  bind_rows(sum_counties, states) %>% 
+    arrange(province, Date)
+    
+}
+
+other_confirmed_long <- function(all_wide, countries = c("Canada")){
+  all_wide %>% 
+    rename(province = "Province/State", 
+           country_region = "Country/Region") %>% 
+    pivot_longer(-c(province, country_region, Lat, Long), 
+                 names_to = "Date", values_to = "cumulative_cases") %>% 
+    filter(country_region %in% countries,
+           # have to trap the rows with missing province (most other countries)
+           # otherwise str_detect(province ...) is missing and dropped by filter()
+           is.na(province) | str_detect(province, "Princess", negate = TRUE)) %>% 
+    mutate(Date = mdy(Date))
+}
